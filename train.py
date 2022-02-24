f5f5d425c111c09b566626f555e44966473d3aca
Risav <risavsingh.saingar@foghorn.io>
2022-02-24 16:51:47 +0530

    train

--- train.py
+++ train.py
@@ -4,9 +4,12 @@ from Data.generator import PreProcessor, Transform
 from Models.losses import calc_quant_loss, adopt_weight, LPIPS, calc_lambda
 from torch.utils.data import DataLoader
 from torchvision import transforms as T
+from glob import glob
+from collections import deque
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import os
 
 class Train:
     def __init__(self, base_channel=128, levels=[1, 1, 2, 2, 4], num_enc_residual_layers=2, num_dec_residual_layers=3, latent_dims=256, num_vectors=1024, num_layers=3, base_filters=64):
@@ -27,44 +30,52 @@ class Train:
 
     def __call__(self):
         step = 0
-        save_every = 1000
+        save_every = 1
+        max_to_keep = 5
         ckpt_dir = "Ckpts/"
-        for img_batch in self.generator:
-            print(img_batch.shape)
-            1/0
-            imgs = torch.tensor(img_batch).permute(0, 3, 1, 2).contiguous()
-            step += 1
-            decoded, z_q_ma, z_q, z, inp_quant = self.vqgan(imgs)
-            quant_loss = calc_quant_loss(z_q, z)
-            disc_real = self.discriminator(imgs)
-            disc_fake = self.discriminator(decoded)
-            perceptual_loss = self.perc_loss(imgs, decoded)
-            rec_loss = torch.abs(imgs - decoded)
-            nll_loss = (perceptual_loss + rec_loss).mean()
-            g_loss = -torch.mean(disc_fake)
+        ckpt_queue = deque()
+        num_epochs = 100
+        for epoch in range(num_epochs):
+            with tqdm(self.generator) as bar:
+                for imgs in self.generator:
+                    step += 1
+                    bar.update(1)
+                    decoded, z_q_ma, z_q, z, inp_quant = self.vqgan(imgs)
+                    quant_loss = calc_quant_loss(z_q, z)
+                    disc_real = self.discriminator(imgs)
+                    disc_fake = self.discriminator(decoded)
+                    perceptual_loss = self.perc_loss(imgs, decoded)
+                    rec_loss = torch.abs(imgs - decoded)
+                    nll_loss = (perceptual_loss + rec_loss).mean()
+                    g_loss = -torch.mean(disc_fake)
 
-            disc_coeff = adopt_weight(step)
-            lmda = 0
-            if disc_coeff > 0:
-                lmda = calc_lambda(nll_loss, g_loss, self.vqgan)
-            vq_loss = nll_loss + quant_loss + disc_coeff * lmda * g_loss
+                    disc_coeff = adopt_weight(step)
+                    lmda = 0
+                    if disc_coeff > 0:
+                        lmda = calc_lambda(nll_loss, g_loss, self.vqgan)
+                    vq_loss = nll_loss + quant_loss + disc_coeff * lmda * g_loss
 
-            d_loss_real = torch.mean(F.relu(1. - disc_real))
-            d_loss_fake = torch.mean(F.relu(1. + disc_fake))
+                    d_loss_real = torch.mean(F.relu(1. - disc_real))
+                    d_loss_fake = torch.mean(F.relu(1. + disc_fake))
 
-            gan_loss = disc_coeff * .5 * (d_loss_real + d_loss_fake)
+                    gan_loss = disc_coeff * .5 * (d_loss_real + d_loss_fake)
 
-            self.opt_vq.zero_grad()
-            vq_loss.backward(retain_graph=True)
+                    self.opt_vq.zero_grad()
+                    vq_loss.backward(retain_graph=True)
 
-            self.opt_disc.zero_grad()
-            gan_loss.backward()
+                    self.opt_disc.zero_grad()
+                    gan_loss.backward()
 
-            self.opt_vq.step()
-            self.opt_disc.step()
+                    self.opt_vq.step()
+                    self.opt_disc.step()
+                    bar.set_postfix({"VQ Loss": np.round(vq_loss.cpu().numpy(), 3), "Gan Loss": np.round(gan_loss.cpu().numpy(), 3)})
 
-            if step % save_every == 0:
-                torch.save(self.vqgan.state_dict(), f"{ckpt_dir}vqgan_{step}.pt")
+                    if step % save_every == 0:
+                        ckpt_name = f"{ckpt_dir}vqgan_{step}.pt"
+                        torch.save(self.vqgan.state_dict(), f"{ckpt_dir}vqgan_{step}.pt")
+                        ckpt_queue.append(ckpt_name)
+                        if len(ckpt_queue) > max_to_keep:
+                            os.remove(ckpt_queue.popleft())
 
 
 if __name__ == "__main__":
